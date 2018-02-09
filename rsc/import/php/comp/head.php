40b1d0f4c753e5ac17eadbc6ca1575dfb2b3221b
Isak Hauge <isakhauge@gmail.com>
2018-02-09 23:20:31 +0100

    Install JQuery. Fix Bootstrap files

--- rsc/import/php/comp/head.php
+++ rsc/import/php/comp/head.php
@@ -20,6 +20,12 @@
 
 <!-- DOCUMENT IMPORT: START-->
 
+    <!-- JQuery 3.3.1 -->
+    <script
+    src="http://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous"></script>
+
 	<!-- Bootstrap 4.0 -->
 	<link rel="stylesheet" type="text/css" href="rsc/bootstrap/css/bootstrap-reboot.css">
 	<link rel="stylesheet" type="text/css" href="rsc/bootstrap/css/bootstrap-grid.css">
