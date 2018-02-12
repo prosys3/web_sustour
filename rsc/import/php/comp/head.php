07004d059898b1eb19679d5164773ff5806c5903
Isak Hauge <isakhauge@gmail.com>
2018-02-12 11:39:31 +0100

    Add white space to code

--- rsc/import/php/comp/head.php
+++ rsc/import/php/comp/head.php
@@ -30,7 +30,7 @@
 
     <!-- JQuery 3.3.1 -->
 
-    <script
+        <script
         src="http://code.jquery.com/jquery-3.3.1.min.js"
         integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
         crossorigin="anonymous"></script>
