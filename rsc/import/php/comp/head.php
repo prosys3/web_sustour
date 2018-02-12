e75d6fc3704ebe73454e19ed9da0fd579813224f
Isak Hauge <isakhauge@gmail.com>
2018-02-12 11:38:29 +0100

    Add white space to code

--- rsc/import/php/comp/head.php
+++ rsc/import/php/comp/head.php
@@ -1,16 +1,23 @@
 <!-- DOCUMENT GENERAL SETTINGS: START -->
 
+
     <!-- Document character: -->
-    <meta charset="UTF-8">
+
+        <meta charset="UTF-8">
+
 
     <!-- Responsive viewport scaling: -->
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, shrink-to-fit=no">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, shrink-to-fit=no">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
 
     <!-- Document author information: -->
-    <meta name="description" content="Sustainable Tourism">
-    <meta name="keywords" content="tourism, sustainable, sustainability, rural, entrepreneurship">
-    <meta name="author" content="ProSys3">
+
+        <meta name="description" content="Sustainable Tourism">
+        <meta name="keywords" content="tourism, sustainable, sustainability, rural, entrepreneurship">
+        <meta name="author" content="ProSys3">
+
 
 <!-- DOCUMENT GENERAL SETTINGS: END -->
 
@@ -20,21 +27,30 @@
 
 <!-- DOCUMENT IMPORT: START-->
 
-    <!-- JQuery 3.3.1 -->
-    <script
-    src="http://code.jquery.com/jquery-3.3.1.min.js"
-    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-    crossorigin="anonymous"></script>
 
-	<!-- Bootstrap 4.0 -->
-    <link rel="stylesheet" type="text/css" href="rsc/bootstrap/css/bootstrap-reboot.css">
-    <link rel="stylesheet" type="text/css" href="rsc/bootstrap/css/bootstrap-grid.css">
-    <link rel="stylesheet" type="text/css" href="rsc/bootstrap/css/bootstrap.css">
+    <!-- JQuery 3.3.1 -->
+
+    <script
+        src="http://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
+
+
+    <!-- Bootstrap 4.0 -->
+
+        <link rel="stylesheet" type="text/css" href="rsc/bootstrap/css/bootstrap-reboot.css">
+        <link rel="stylesheet" type="text/css" href="rsc/bootstrap/css/bootstrap-grid.css">
+        <link rel="stylesheet" type="text/css" href="rsc/bootstrap/css/bootstrap.css">
+
 
     <!-- ProSys3 CSS -->
-    <!-- Own CSS here... -->
+
+        <!-- Own CSS here... -->
+
 
     <!-- ProSys3 JavaScript -->
-    <!-- Own JS here... -->
+
+        <!-- Own JS here... -->
+
 
 <!-- DOCUMENT IMPORT: END -->
