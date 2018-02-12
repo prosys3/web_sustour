75d5bd45678560181ffa27d17cdafcef9e60055f
AndersKoo-PC\Anders Koo <anderskoo@live.no>
2018-02-12 17:22:47 +0000

    Lagt til header og footer. Disse kan endres om det er ønskelig.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<title></title>
 </head>
 <body>
-	<nav class="navbar navbar-inverse">
+<nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
       <a class="navbar-brand" href="#">WebSiteName</a>
@@ -15,20 +15,147 @@
       <li><a href="#">Page 1</a></li>
       <li><a href="#">Page 2</a></li>
     </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-    </ul>
+    <form class="navbar-form navbar-left" action="/action_page.php">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="Search">
+      </div>
+      <button type="submit" class="btn btn-default">Submit</button>
+    </form>
   </div>
 </nav>
-<div class="container">
-  <div class="jumbotron">
-    <h1>Bootstrap Tutorial</h1> 
-    <p>Bootstrap is the most popular HTML, CSS, and JS framework for developing
-    responsive, mobile-first projects on the web.</p> 
+</nav>
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-3">SUSTAINABLE TOURISM</h1>
+    <p class="lead">RURAL ENTREPRENEURSHIP AND HERITAGE</p>
   </div>
-  <p>This is some text.</p> 
-  <p>This is another text.</p> 
+</div>. 
+  </div>
+  <div class="row">
+  <div class="col-sm-6">
+
+    <h2>News</h2>  
+    <p>Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet, da en ukjent boktrykker stokket en mengde bokstaver for å lage et prøveeksemplar av en bok.</p> 
+
+  <!-- quick fix, må gjøre noe annet css start -->
+  </br>
+  </br>
+  <!-- quick fix, må gjøre noe annet css stop -->
+
+  <button type="button" class="btn btn-success">Read more</button>
+  </div>
+  <div class="col-sm-6">
+    <h2>News</h2>  
+      <p>Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet, da en ukjent boktrykker stokket en mengde bokstaver for å lage et prøveeksemplar av en bok.
+      </p>
+  </br>
+  </br>
+  <button type="button" class="btn btn-success">Read more</button></div>
+
 </div>
+  
+</div>
+<img src="rsc\landingpage\images\1.jpg" class="rounded-circle">
+<!-- Footer start -->
+                
+<!--Footer-->
+<footer class="page-footer center-on-small-only blue-grey lighten-5 pt-0">
+
+    <!-- <div style="background-color: #21d192;"> -->
+        <!--  <div class="container"> -->
+            <!--Grid row-->
+            <!-- <div class="row py-4 d-flex align-items-center"> -->
+
+                <!-- Grid column -->
+                <!-- <div class="col-12 col-md-5 text-left mb-4 mb-md-0"> -->
+                    <!-- <h6 class="mb-0 white-text text-center text-md-left"><strong>Get connected with us on social networks!</strong></h6> -->
+                <!-- </div> -->
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-12 col-md-7 text-center text-md-right">
+                    <!--Facebook-->
+                    <a class="icons-sm fb-ic ml-0"><i class="fa fa-facebook white-text mr-lg-4"> </i></a>
+                    <!--Twitter-->
+                    <a class="icons-sm tw-ic"><i class="fa fa-twitter white-text mr-lg-4"> </i></a>
+                    <!--Google +-->
+                    <a class="icons-sm gplus-ic"><i class="fa fa-google-plus white-text mr-lg-4"> </i></a>
+                    <!--Linkedin-->
+                    <a class="icons-sm li-ic"><i class="fa fa-linkedin white-text mr-lg-4"> </i></a>
+                    <!--Instagram-->
+                    <a class="icons-sm ins-ic"><i class="fa fa-instagram white-text mr-lg-4"> </i></a>
+                </div>
+                <!--Grid column-->
+
+            </div>
+            <!--Grid row-->
+        </div>
+    </div>
+
+    <!--Footer Links-->
+    <div class="container mt-5 mb-4 text-center text-md-left">
+        <div class="row mt-3">
+
+            <!--First column-->
+            <div class="col-md-3 col-lg-4 col-xl-3 mb-r dark-grey-text">
+                <h6 class="title font-bold"><strong>SUSTAINABLE TOURISM</strong></h6>
+                <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <p>Lorem Ipsum er rett og slett dummytekst fra og for trykkeindustrien. Lorem Ipsum har vært bransjens standard for dummytekst helt siden 1500-tallet, da en ukjent boktrykker stokket en mengde bokstaver for å lage et prøveeksemplar av en bok..
+                </p>
+            </div>
+            <!--/.First column-->
+
+            <!--Second column-->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-r dark-grey-text">
+                <h6 class="title font-bold"><strong>Services we offer</strong></h6>
+                <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <p><a href="#!" class="dark-grey-text">MDBootstrap</a></p>
+                <p><a href="#!" class="dark-grey-text">MDWordPress</a></p>
+                <p><a href="#!" class="dark-grey-text">BrandFlow</a></p>
+                <p><a href="#!" class="dark-grey-text">Bootstrap Angular</a></p>
+            </div>
+            <!--/.Second column-->
+
+            <!--Third column-->
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-r dark-grey-text">
+                <h6 class="title font-bold"><strong>Visit us on</strong></h6>
+                <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <p><a href="#!" class="dark-grey-text">Your Account</a></p>
+                <p><a href="#!" class="dark-grey-text">Become an Affiliate</a></p>
+                <p><a href="#!" class="dark-grey-text">Shipping Rates</a></p>
+                <p><a href="#!" class="dark-grey-text">Help</a></p>
+            </div>
+            <!--/.Third column-->
+
+            <!--Fourth column-->
+            <div class="col-md-4 col-lg-3 col-xl-3 dark-grey-text">
+                <h6 class="title font-bold"><strong>Contact us on</strong></h6>
+                <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                <p><i class="fa fa-home mr-3"></i> New York, NY 10012, US</p>
+                <p><i class="fa fa-envelope mr-3"></i> info@example.com</p>
+                <p><i class="fa fa-phone mr-3"></i> + 01 234 567 88</p>
+                <p><i class="fa fa-print mr-3"></i> + 01 234 567 89</p>
+            </div>
+            <!--/.Fourth column-->
+
+        </div>
+    </div>
+    <!--/.Footer Links-->
+
+    <!-- Copyright-->
+    <div class="footer-copyright">
+        <div class="container-fluid">
+            © 2017 Copyright: <a href="https://www.MDBootstrap.com"><strong> MDBootstrap.com</strong></a>
+        </div>
+    </div>
+    <!--/.Copyright -->
+    
+</footer>
+<!--/.Footer-->
+                
+
+<!-- Footer start -->
+<?php include 'rsc/import/php/footer.php'; ?>
+<!-- Footer end -->
 </body>
 </html>
--- rsc/import/php/head.php
+++ rsc/import/php/head.php
@@ -1,24 +1,36 @@
+
+
 <!-- Document settings: start -->
 <meta charset="UTF-8">
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
 <meta name="description" content="SUSTAINABLE TOURISM">
 <meta name="keywords" content="TOURISM,SUSTAINABILITY, RURAL ENTREPRENEURSHIP	">
 <meta name="author" content="ProSys3">
 <!-- Document settings: end -->
 
+
 <!-- Document import: start -->
 	<!-- CSS -->
-	<link rel="stylesheet" type="text/css" href="rsc/bootstrap/css/bootstrap-reboot.css">
-	<link rel="stylesheet" type="text/css" href="rsc/bootstrap/css/bootstrap-reboot.min.css">
-	<link rel="stylesheet" type="text/css" href="rsc/bootstrap/css/bootstrap-grid.css">
-	<link rel="stylesheet" type="text/css" href="rsc/bootstrap/css/bootstrap-grid.min.css">
-	<link rel="stylesheet" type="text/css" href="rsc/bootstrap/css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="rsc/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="rsc\bootstrap\bootstrap-4.0.0-dist\css\bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="rsc\bootstrap\bootstrap-4.0.0-dist\css\bootstrap.css.map">
+	<link rel="stylesheet" type="text/css" href="rsc\bootstrap\bootstrap-4.0.0-dist\css\bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="rsc\bootstrap\bootstrap-4.0.0-dist\css\bootstrap.min.css.map">
+	<link rel="stylesheet" type="text/css" href="rsc\bootstrap\bootstrap-4.0.0-dist\css\bootstrap-grid.css">
+	<link rel="stylesheet" type="text/css" href="rsc\bootstrap\bootstrap-4.0.0-dist\css\bootstrap-grid.min.css">
+	<link rel="stylesheet" type="text/css" href="rsc\bootstrap\bootstrap-4.0.0-dist\css\bootstrap-grid.min.css.map">
+	<link rel="stylesheet" type="text/css" href="rsc\bootstrap\bootstrap-4.0.0-dist\css\bootstrap-reboot.css">
+	<link rel="stylesheet" type="text/css" href="rsc\bootstrap\bootstrap-4.0.0-dist\css\bootstrap-reboot.css.map">
+	<link rel="stylesheet" type="text/css" href="rsc\bootstrap\bootstrap-4.0.0-dist\css\bootstrap-reboot.min.css.map">
 	<!-- JS -->
-	<script type="text/javascript" src="rsc/bootstrap/js/bootstrap.bundle.js"></script>
-	<script type="text/javascript" src="rsc/bootstrap/js/bootstrap.bundle.min.js"></script>
-	<script type="text/javascript" src="rsc/bootstrap/js/bootstrap.js"></script>
-	<script type="text/javascript" src="rsc/bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="rsc\bootstrap\bootstrap-4.0.0-dist\js\bootstrap.bundle.js"></script>
+	<script type="text/javascript" src="rsc\bootstrap\bootstrap-4.0.0-dist\js\bootstrap.bundle.js.map"></script>
+	<script type="text/javascript" src="rsc\bootstrap\bootstrap-4.0.0-dist\js\bootstrap.bundle.min.js"></script>
+	<script type="text/javascript" src="rsc\bootstrap\bootstrap-4.0.0-dist\js\bootstrap.bundle.min.js.map"></script>
+	<script type="text/javascript" src="rsc\bootstrap\bootstrap-4.0.0-dist\js\bootstrap.bundle.min.js"></script>
+	<script type="text/javascript" src="rsc\bootstrap\bootstrap-4.0.0-dist\js\bootstrap.bundle.min.js.map"></script>
+	<script type="text/javascript" src="rsc\bootstrap\bootstrap-4.0.0-dist\js\bootstrap.js"></script>
+	<script type="text/javascript" src="rsc\bootstrap\bootstrap-4.0.0-dist\js\bootstrap.js.map"></script>
 <!-- Document import: end -->
+
+
