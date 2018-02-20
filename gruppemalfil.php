60a9fdb9a59cf3ace41fea2e0f8ec8a4a57a0bd2
216760 <andreas_k83@hotmail.com>
2018-02-20 15:49:38 +0100

    fix gruppemalfil.php

--- gruppemalfil.php
+++ gruppemalfil.php
@@ -14,88 +14,74 @@
 #### DO NOT EDIT THE HEAD IMPORT ABOVE
 #########################################################
 -->
-
-<!-- ############################################################################## -->
-    <!-- DOCUMENT TITLE: -->
-    <!-- Header start -->
     <title>Hovedside</title>
 
 </head>
 <body>
 
-  <!-- Navbar start -->
-  <!-- MÅ SKIFTE FARGE -->
+
+<!--
+#########################################################
+#### DO NOT EDIT THE NAVIGATION IMPORT BELOW
+#########################################################
+-->
     <?php include 'rsc/import/php/comp/header.php' ?>
-
-<!-- MÅ SKIFTE FARGE -->
-<!-- Navbar start -->
-
-
-<div class="jumbotron jumbotron-fluid">
-  <div class="container">
-
-
-
-    <!-- logo navbar start -->
-    <a href="#" class=""><img src="rsc/img/carousel/wallpaper.jpg" width="300" height="250"></a>
-    <!-- logo navbar stop -->
-
-    <h1 class="display-3">EGET INNHOLD</h1>
-    <p class="lead">EGET INNHOLD</p>
-
-  </div>
-</div>
-<!-- Header stop -->
-<!-- ############################################################################## -->
+<!--
+#########################################################
+#### DO NOT EDIT THE NAVIGATION IMPORT ABOVE
+#########################################################
+-->
 
 
 
 
-<!-- ############################################################################## -->
-<!-- Avsnittene start -->
-
-<div class="container">
-
-  <div class="row">
-  <div class="col-sm">
-
-    <h2>EGET INNHOLD</h2>  
-    <p>EGET INNHOLD</p> 
-
-  <!-- quick fix, må gjøre noe annet css start -->
-  </br>
-  </br>
-  <!-- quick fix, må gjøre noe annet css stop -->
-
-  
-  <!-- Button start -->
-  <button type="button" class="btn btn-success">Read more</button>
-  <!-- Button stop -->
-
-  </div>
-  <div class="col-sm">
-    <h2>EGET INNHOLD</h2>  
-      <p>EGET INNHOLD
-      </p>
-  </br>
-  </br>
-  <button type="button" class="btn btn-success">Read more</button></div>
-
-</div>
 
 
-<!-- Avnitt stop -->
-<!-- ############################################################################## -->
 
 
-<!-- Footer start -->
-  <?php include 'rsc/mport/php/comp/footer.php' ?>
 
-<!--Footer stop-->
 
-<!--/.Footer-->
+
+<!--
+#########################################################
+################## MAIN CONTENT: START ##################
+#########################################################
+-->
+
+
+  <!-- DOCUMENT MAIN CONTENT: -->
+  <!-- Your code goes here ... -->
+
+
+<!--
+#########################################################
+################## MAIN CONTENT: END ##################
+#########################################################
+-->
+
+
+
+
+
+
+
+
+
+
+
+<!--
+#########################################################
+#### DO NOT EDIT THE FOOTER IMPORT BELOW
+#########################################################
+-->
+  <!-- DOCUMENT FOOTER: -->
+  <?php include 'rsc/import/php/comp/footer.php' ?>
+
+<!--
+#########################################################
+#### DO NOT EDIT THE FOOTER IMPORT ABOVE
+#########################################################
+-->
                 
-
-
 </body>
 </html>
