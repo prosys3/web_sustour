c39249c9bdaa3dbfa56d1b11aae75c0ccd942bc7
Kim V Pedersen <telly1337@gmail.com>
2018-04-11 22:43:46 +0200

    opprydding

--- rsc/import/php/components/header.php
+++ rsc/import/php/components/header.php
@@ -16,25 +16,8 @@
                 <li class="nav-item"><a class="nav-link" href="contact.php">Contact us</a></li>
             </ul>
 
-            <div class="dropdown">
-                <button type="button" class="btn btn-secondary">Login</button>
-                <div id="prosys3-dropdown-login" class="dropdown-menu text-center" aria-labelledby="dropdownMenuButton">
-                    <form class="form-signin" action="../../../../../web_sustour/admin/Login.php" method="post">
-                        <img class="mb-4" src="https://getbootstrap.com/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-                        <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-                        <label for="inputEmail" class="sr-only">Email address</label>
-                        <input name="email" type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-                        <label for="inputPassword" class="sr-only">Password</label>
-                        <input name="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-                        <div class="checkbox mb-3">
-                            <label>
-                                <input type="checkbox" value="remember-me"> Remember me
-                            </label>
-                        </div>
-                        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-                    </form>
-                </div>
-            </div>
+            <button type="button" class="btn btn-secondary">Login</button>
+
 
         </div>
 
