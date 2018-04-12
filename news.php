591a4319a4e11641df7c88a508098059a565561f
Anders Koo <anderskoo@live.no>
2018-04-12 21:45:16 +0200

    quick fix
    quick fix

--- news.php
+++ news.php
@@ -33,29 +33,7 @@
   </head>
 
   <body>
-
-    <header>
-      <!-- <div class="collapse bg-dark" id="navbarHeader">
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-8 col-md-7 py-4">
-              <h4 class="text-white">About</h4>
-              <p class="text-muted">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
-            </div>
-            <div class="col-sm-4 offset-md-1 py-4">
-              <h4 class="text-white">Contact</h4>
-              <ul class="list-unstyled">
-                <!-- <li><a href="#" class="text-white">Follow on Twitter</a></li>
-                <li><a href="#" class="text-white">Like on Facebook</a></li>
-                <li><a href="#" class="text-white">Email me</a></li> -->
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div> -->
-     
-    </header>
-
+    
     <main role="main">
 
       <section class="jumbotron text-center">
