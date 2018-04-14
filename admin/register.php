c142eee84b73b551f68a0ef03cdb1172734e17e1
Anders Koo <anderskoo@live.no>
2018-04-15 01:21:54 +0200

    gjort tekstfeltene er smalere

--- admin/register.php
+++ admin/register.php
@@ -23,60 +23,67 @@
 
     <!-- Custom styles for this template -->
     <link href="register.css" rel="stylesheet">
-  </head>
+   </head>
 
-  <body class="text-center">
-    <form class="form-signin" action="admin/registerscript.php" method="post">
-    <img class="mb-2" src="rsc/img/logo/sustour/logo_symbol_green.svg" alt="" width="120" height="120">
+    <!-- Register account start -->
+    <form class="form-signin " action="admin/registerscript.php" method="post">
       <br>
-      <h1 class="h3 mb-3 font-weight-normal">Register account</h1>
+        <h1 class="h3 mb-3 font-weight-normal text-center">Register account</h1>
       <br>
-       <!-- First name start -->
-      <label for="inputEmail" class="sr-only">First name</label>
-	    <input type="text" name="text" id="inputEmail" class="form-control" placeholder="Enter first name" required autofocus>
-       <!-- First name start -->
-      <br>
+    <!-- Register account stop -->
 
-      <!-- Last name start -->
-      <label for="inputEmail" class="sr-only">Last name</label>
-      <input type="text" name="text" id="text" class="form-control" placeholder="Enter last name" required autofocus>
+       <!-- First name start -->
+      <div class="form-group row">
+        <div class="col-xs-3 text-center">
+          <label for="inputEmail" class="sr-only">First name</label>
+    	    <input type="text" name="text" id="inputEmail" class="form-control" placeholder="Enter first name" required autofocus>
+           <!-- First name start -->
+          <br>
+
+          <!-- Last name start -->
+          <label for="inputEmail" class="sr-only">Last name</label>
+          <input type="text" name="text" id="text" class="form-control" placeholder="Enter last name" required autofocus>
       <!-- Last name stop -->
 
       <!-- Password start -->
-      <label for="inputPassword" class="sr-only">Password</label>
-      <br>
-      <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Enter password" required>
-      <div class="checkbox mb-3">
+        <label for="inputPassword" class="sr-only">Password</label>
+        <br>
+        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Enter password" required>
+        <div class="checkbox mb-3">
        <!-- Password stop -->
-      
+       <br>
       <!--UserType start -->
       <div class="form-row align-items-center">
-    <div class="col-auto my-1">
-      <label class="mr-sm-2" for="inlineFormCustomSelect">UserType</label>
-      <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-        <option selected>Please choose...</option>
-        <option value="1">UserTypeOne</option>
-        <option value="2">UserTypeTwo</option>
-        <option value="3">UserTypeThree</option>
-      </select>
-    </div>
+      <div class="col-auto my-1">
+        <label class="mr-sm-2" for="inlineFormCustomSelect">UserType</label>
+        <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+          <option selected>Please choose...</option>
+          <option value="1">UserTypeOne</option>
+          <option value="2">UserTypeTwo</option>
+          <option value="3">UserTypeThree</option>
+        </select>
+      </div>
      <!--UserType stop -->
-
+      <br>
 
       <!-- Email start -->
       <label for="inputEmail" class="sr-only">Email</label>
       <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
       <!-- Email stop -->
-
-
+      <br>
+      <br>
     <!-- Remember me start -->
         <label>
           <input type="checkbox" value="remember-me">Remember me
         </label>
       </div>
+       <br>
+        <br>
     <!-- Remember me stop -->
 
       <button type="submit" class="btn btn-success btn-lg">Login</button>
+       <br>
+       <br>
       <p class="mt-5 mb-3 text-muted"> &copy; 2017-2018 Sustainable Tourism</p>
     </form>
   </body>
