799f8582d13049ff5741e8ef3932e7fe7b5c69b3
Isak Hauge <isakhauge@gmail.com>
2018-04-19 18:50:20 +0200

    Fix news page

--- news.php
+++ news.php
@@ -1,4 +1,5 @@
 <?php  session_start(); ?>
+<?php include 'dbconfig.php' ?>
 <?php include 'rsc/import/php/components/head.php' ?>
 <?php include 'rsc/import/php/components/header.php' ?>
 
@@ -12,188 +13,56 @@
 -->
 
 
-
 <main>
 
-    <div class="jumbotron jumbotron-fluid">
-      <div class="container text-center">
+    <section class="bg-light py-5">
 
-        <!-- logo navbar start -->
-        <a href="#" class=""><img src="rsc/img/logo/sustour/logo_symbol_black.svg" width="150"></a>
-        <!-- logo navbar stop -->
+        <div class="container">
 
-        <!-- logo tekst start -->
-        <h1 class="display-3">Project News</h1>
-        <p class="lead text-muted">Articles for Sustainable Tourism will be available here</p>
-        <!-- logo tekst stopp -->
+            <h1>News</h1>
 
-      </div>
-    </div>
-  
-    <div class="album py-5 bg-light">
-      <div class="container">
-
-        <div class="row">
-          <div class="col-md-4">
-            <div class="card mb-4 box-shadow">
-              <img class="card-img-top" data-src="web_sustour/rsc/img/Thumbnail/card.svg" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">This is an article....</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
-                  </div>
-                  <!-- <small class="text-muted">9 mins</small> -->
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="card mb-4 box-shadow">
-              <img class="card-img-top" data-src="web_sustour/rsc/img/Thumbnail/card.svg" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">This is an article....</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
-                  </div>
-                  <!-- <small class="text-muted">9 mins</small> -->
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="card mb-4 box-shadow">
-              <img class="card-img-top" data-src="web_sustour/rsc/img/Thumbnail/card.svg" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">This is an article....</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
-                  </div>
-                  <!-- <small class="text-muted">9 mins</small> -->
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-4">
-            <div class="card mb-4 box-shadow">
-              <img class="card-img-top" data-src="web_sustour/rsc/img/Thumbnail/card.svg" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">This is an article...</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
-                  </div>
-                  <!-- <small class="text-muted">9 mins</small> -->
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="card mb-4 box-shadow">
-              <img class="card-img-top" data-src="web_sustour/rsc/img/Thumbnail/card.svg" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">This is an article...</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
-                  </div>
-                  <!-- <small class="text-muted">9 mins</small> -->
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="card mb-4 box-shadow">
-              <img class="card-img-top" data-src="web_sustour/rsc/img/Thumbnail/card.svg" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">This is an article...</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
-                  </div>
-                  <!-- <small class="text-muted">9 mins</small> -->
-                </div>
-              </div>
-             </div>
-          </div>
-
-          <div class="col-md-4">
-            <div class="card mb-4 box-shadow">
-              <img class="card-img-top" data-src="web_sustour/rsc/img/Thumbnail/card.svg" alt="Card image cap">
-              <div class="card-body">
-                 <p class="card-text">This is an article...</p>
-                 <div class="d-flex justify-content-between align-items-center">
-                   <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                     <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
-                  </div>
-                  <!-- <small class="text-muted">9 mins</small> -->
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="card mb-4 box-shadow">
-              <img class="card-img-top" data-src="web_sustour/rsc/img/Thumbnail/card.svg" alt="Card image cap">
-              <div class="card-body">
-                 <p class="card-text">This is an article...</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
-                  </div>
-                  <!-- <small class="text-muted">9 mins</small> -->
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-4">
-            <div class="card mb-4 box-shadow">
-              <img class="card-img-top" data-src="web_sustour/rsc/img/Thumbnail/card.svg" alt="Card image cap">
-              <div class="card-body">
-                <p class="card-text">This is an article...</p>
-                <div class="d-flex justify-content-between align-items-center">
-                  <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <!-- <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
-                  </div>
-                 <!-- <small class="text-muted">9 mins</small> -->
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
 
-    <footer class="text-muted">
-      <div class="container">
-        <p class="float-right">
-          <a href="#">Back to top</a>
-        </p>
-      </div>
-    </footer>
+    </section>
+
+
+    <section class="py-5">
+
+        <div class="container d-flex flex-row">
+
+            <?php
+
+                $sql = 'SELECT * FROM Post ORDER BY Post_Date_Created DESC';
+                $result = mysqli_query($con, $sql);
+
+                while ( $row = mysqli_fetch_array( $result ) ){
+
+                    echo '<div class="card" style="width: calc(33.33% - 10px); margin: 5px;">';
+                    if ($row['Post_Image_Featured'] != null){
+                        echo '<img class="card-img-top" src="'.$row['Post_Image_Featured'].'" alt="Card image cap">';
+                    }
+                    echo '<div class="card-body d-flex flex-column justify-content-between" style="flex-wrap: wrap; ">';
+                    echo '<h5 class="card-title">';
+                    echo $row['Post_Title'];
+                    echo '</h5>';
+                    echo '<div class="d-flex flex-column">';
+                    echo '<a href="news.php?post='.$row['Post_ID'].'" class="btn btn-primary">Read post</a>';
+                    if ( $row['Post_Private'] == 1 ) {
+                        echo '<span class="badge badge-secondary mt-3">Private</span>';
+                    }
+                    echo '</div>';
+                    echo '</div>';
+                    echo '</div>';
+
+                }
+
+            ?>
+
+        </div>
+
+    </section>
 
 </main>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="../../../../assets/js/vendor/popper.min.js"></script>
-    <script src="../../../../dist/js/bootstrap.min.js"></script>
-    <script src="../../../../assets/js/vendor/holder.min.js"></script> -->
-
 
 
 <!--
