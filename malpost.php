9465059d2f686d1a737e49b378618d19affffa95
216760 <andreas_k83@hotmail.com>
2018-04-25 16:39:41 +0200

    Lagde mal for events og post

---
+++ malpost.php
@@ -0,0 +1,79 @@
+<?php
+
+    // Preliminary code:
+    include 'dbconfig.php';
+    include 'admin/rsc/import/php/functions/functions.php';
+
+    // Regular template:
+    include 'rsc/import/php/components/head.php';
+    include 'rsc/import/php/components/header.php';
+
+?>
+
+<!--
+##################################################################################
+######################## ! DO NOT EDIT ABOVE THIS POINT ! ########################
+##################################################################################
+-->
+
+
+
+
+
+
+<!-- Main er obligatorisk -->
+<main>
+	<section class="py-5">
+		<div class="container d-flex flex-row">
+		  <div class="col-sm-6">
+		    <div class="card">
+		     <img class="card-img-top" src="..." alt="Card image cap">
+		      <div class="card-body">
+		        <h5 class="card-title">Special title treatment</h5>
+		        <p class="card-text">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.</p>
+		        <a href="#" class="btn btn-primary">Go somewhere</a>
+		      </div>
+		    </div>
+		  </div>
+		  <div class="col-sm-6">
+		    <div class="card">
+		     <img class="card-img-top" src="..." alt="Card image cap">
+		      <div class="card-body">
+		        <h5 class="card-title">Special title treatment</h5>
+		        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+		        <a href="#" class="btn btn-primary">Go somewhere</a>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+	<section class="container py-5">
+	    <div class="row">
+		    <div class="col text-center">
+			  	<p>
+					<a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+				    	Link with href
+					</a>
+				</p>
+				<div class="collapse" id="collapseExample">
+				  	<div class="card card-body">
+				    	Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+			  	  	</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</main>
+
+
+
+
+
+
+
+<!--
+##################################################################################
+######################## ! DO NOT EDIT BELOW THIS POINT ! ########################
+##################################################################################
+-->
+
+<?php include 'rsc/import/php/components/footer.php' ?>
