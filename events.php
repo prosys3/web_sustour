72a222dc4dad7dcb25b15e4ef9b25778243b0946
BertBert0 <31742102+BertBert0@users.noreply.github.com>
2018-04-27 00:30:04 +0200

    Preliminary event.php
    Waiting for front-end to complete

--- events.php
+++ events.php
@@ -35,20 +35,19 @@
     </section>
 
 
-    <section class="py-5">
+    
 
-        <div class="container">
 
             <?php
 
-            $sql = 'SELECT * FROM Event ORDER BY Event_Date ASC';
+            $sql = 'SELECT * FROM Event WHERE Event_Date > CURDATE() ORDER BY Event_Date ASC';
             $result = mysqli_query($con, $sql);
 
             $counter = 1;
-                          
-            echo '<div class="row">';
+            echo '<section class="py-4">';              
+            echo '<div class="card-deck mx-auto w-50">';
             while ( $row = mysqli_fetch_array( $result ) ){
-                $tempQuery = "SELECT * FROM Company WHERE " . $row['Event_Company'] . " = Company_ID ";
+                $tempQuery = "SELECT * FROM Company WHERE " . $row['Event_Company'] . " = Company_ID";
                 $tempRes = mysqli_query($con,$tempQuery);
                 $tempRow = mysqli_fetch_array($tempRes);
                 // Gets date
@@ -62,33 +61,37 @@
                 $endTime = $eTime->format('H:i');
                
 
-                echo '<div class="col-sm">';
-                echo '<div class="card" style="width: 20rem; margin:10px;">';
-                echo '<div class="card-body d-flex flex-column justify-content-between" style="flex-wrap: wrap; ">';
-                echo '<h5 class="card-title" style="text-align: center;">';
+                echo '<div class="card border-dark">';
+                echo '<div class="card-body d-flex flex-column">';
+                echo '<h5 class="card-title text-center">';
                 echo $row['Event_Name'];
                 echo '</h5>';
-                echo '<h6 class="card-subtitle mb-2">' . $tempRow['Company_Name'];
+                echo '<h6 class="card-subtitle">' . $tempRow['Company_Name'];
                 echo '</h6>';
-                echo '<h7 class="card-subtitle mb-2 text-muted">' . $row['Event_Location'] ;
-                echo '</h7>';
-                echo '<p class="card-text">' . $date;
-                echo '</br>' . $startTime . ' - ' . $endTime;
-                echo '</p>' ;
-                echo '<div class="card-text">';
-                echo '<p>' . $row['Event_Text'] . '</p>';
-                echo '</div>';
-                echo '</div>';
+                echo '<h6 class="card-subtitle my-2 text-muted">' . $row['Event_Location'] ;
+                echo '</h6>';
+                echo '<ul class="list-group list-group-flush">';
+                echo '<li class="list-group-item">' . $date;
+                echo '</li>';
+                echo '<li class="list-group-item">' . $startTime . ' - ' . $endTime;
+                echo '</li>' ;
+                echo '</ul>' ;
+                echo '<p class="card-text">'. $row['Event_Text'];
+                echo '</p>';
                 echo '</div>';
                 echo '</div>';
               
                 if ($counter % 3 == 0) {
                  echo '</div>';
-                 echo '<div class="row">';
+                 echo '</section>';
+                 echo '<section class="py-4">';
+                 echo '<div class="card-deck mx-auto w-50">';
                   }
                   $counter++;
 
             }
+                 echo '</div>';
+                 echo '</section>';
 
             ?>
 
@@ -96,6 +99,80 @@
 
     </section>
 
+     <!-- Collapse button start -->
+  <section class="container py-5">
+      <div class="row">
+          <div class="col text-center">
+              <p>
+                  <a class="btn btn-dark" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                        View completed events
+                  </a>
+                </p>
+                <div class="collapse" id="collapseExample">
+          <div class="card text-white bg-dark">
+                  <div class="card card-header">
+                  COMPLETED EVENTS
+                </div>
+          </div>
+  <!-- Collapse button end -->
+
+   <!-- Collapse content start -->
+          <?php
+
+            $sql_old = 'SELECT * FROM Event WHERE Event_Date < CURDATE() ORDER BY Event_Date ASC';
+            $result_old = mysqli_query($con, $sql_old);
+
+            $counter_old = 1;
+            echo '<section class="py-3">';              
+            echo '<div class="card-deck mx-auto w-100">';
+            while ( $row_old = mysqli_fetch_array( $result_old ) ){
+                $tempQuery = "SELECT * FROM Company WHERE " . $row_old['Event_Company'] . " = Company_ID";
+                $tempRes = mysqli_query($con,$tempQuery);
+                $tempRow = mysqli_fetch_array($tempRes);
+                // Gets date
+                $d = new DateTime($row_old['Event_Date']);
+                $date = $d->format('F jS - o');
+                
+                // Gets time
+                $sTime = new DateTime($row_old['Event_Start']);
+                $eTime = new DateTime($row_old['Event_End']);
+                $startTime = $sTime->format('H:i');
+                $endTime = $eTime->format('H:i');
+               
+
+                echo '<div class="card border-dark">';
+                echo '<div class="card-body d-flex flex-column">';
+                echo '<h5 class="card-title text-center">';
+                echo $row_old['Event_Name'];
+                echo '</h5>';
+                echo '<h6 class="card-subtitle">' . $tempRow['Company_Name'];
+                echo '</h6>';
+                echo '<h6 class="card-subtitle my-2 text-muted">' . $row_old['Event_Location'] ;
+                echo '</h6>';
+                echo '<ul class="list-group list-group-flush">';
+                echo '<li class="list-group-item">' . $date;
+                echo '</li>';
+                echo '<li class="list-group-item">' . $startTime . ' - ' . $endTime;
+                echo '</li>' ;
+                echo '</ul>' ;
+                echo '<p class="card-text">'. $row_old['Event_Text'];
+                echo '</p>';
+                echo '</div>';
+                echo '</div>';
+              
+                if ($counter_old % 3 == 0) {
+                 echo '</div>';
+                 echo '</section>';
+                 echo '<section class="py-3">';
+                 echo '<div class="card-deck mx-auto w-100">';
+                  }
+                  $counter_old++;
+
+            }
+                 echo '</div>';
+                 echo '</section>';
+
+            ?>
 </main>
 <!-- quick fix -->
 <br>
