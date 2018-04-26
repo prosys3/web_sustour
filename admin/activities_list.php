c1d8fde8b2b3978388ca9c3e80abd82b8eae7fa4
BertBert0 <31742102+BertBert0@users.noreply.github.com>
2018-04-26 14:51:14 +0200

    Bugfix - kim sux

--- admin/activities_list.php
+++ admin/activities_list.php
@@ -8,7 +8,7 @@ include 'rsc/import/php/functions/functions.php';
 if( !isset($_SESSION['login']) ){
     header("Location:../login.php")
     ;exit();
-} elseif ($_SESSION['user_type'] < 4) {
+} elseif ($_SESSION['user_type'] > 3) {
     header("Location:../index.php")
     ;exit();
 }
