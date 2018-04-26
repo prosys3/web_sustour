38167f18a4a2ab39800273d327e082f047419b26
Anders Koo <anderskoo@live.no>
2018-04-26 20:13:18 +0200

    Gjort malpost rammme og knapper grå

--- malpost.php
+++ malpost.php
@@ -27,45 +27,25 @@
   <!-- Cards start -->
 	<section class="p-5">
 	 <div class="card-deck mx-auto w-50">
-    <div class="card">
+    <div class="card border-dark">
       <img class="card-img-top mx-auto d-block" src="rsc/img/Post/feels-good-man.png" alt="Card image cap">
         <div class="card-body d-flex flex-column">
           <h5 class="card-title">Card title</h5>
           <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <a href="#" class="btn btn-primary mt-auto">Go somewhere</a>
+          <a href="#" class="btn btn-dark mt-auto">Go somewhere</a>
         </div>
     </div>
-    <div class="card">
+    <div class="card border-dark">
       <img class="card-img-top mx-auto d-block" src="rsc/img/Post/feels-good-man.png" alt="Card image cap">
         <div class="card-body d-flex flex-column">
           <h5 class="card-title">Card title</h5>
           <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary mt-auto">Go somewhere</a>
+          <a href="#" class="btn btn-dark mt-auto">Go somewhere</a>
         </div>
     </div>
   </section>
   <!-- Cards stop -->
-
-  <!-- Button start -->
-  <section class="container py-5">
-	  <div class="row">
-		  <div class="col text-center">
-			  <p>
-				  <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-				    	Link with href
-				  </a>
-				</p>
-				<div class="collapse" id="collapseExample">
-				  <div class="card card-body">
-				    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
-			  	</div>
-				</div>
-			</div>
-		</div>
-	</section>
-  <!-- Button stop -->
-
-
+  
 </main>
 
 
